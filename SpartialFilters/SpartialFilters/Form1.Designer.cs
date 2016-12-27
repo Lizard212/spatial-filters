cf56ce36a73907c5a42431501055e3f4846b8ac8
Your Name <you@example.com>
2016-12-27 18:45:21 +0700

    SpartialFilters\SpartialFilters\Form1.Designer.cs SpartialFilters\SpartialFilters\Sharpening.cs SpartialFilters\SpatialFiltersCpp\CMakeLists.txt SpartialFilters\SpatialFiltersCpp\iio.c SpartialFilters\SpatialFiltersCpp\iio.h SpartialFilters\SpatialFiltersCpp\libauxiliar.cpp SpartialFilters\SpatialFiltersCpp\libauxiliar.h SpartialFilters\SpatialFiltersCpp\libdenoising.cpp SpartialFilters\SpatialFiltersCpp\libdenoising.h SpartialFilters\SpatialFiltersCpp\nlmeans.cpp

---
+++ SpartialFilters/SpartialFilters/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SpartialFilters
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_browser = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox_src = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_src)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_browser
+            // 
+            this.btn_browser.Location = new System.Drawing.Point(56, 362);
+            this.btn_browser.Name = "btn_browser";
+            this.btn_browser.Size = new System.Drawing.Size(75, 23);
+            this.btn_browser.TabIndex = 0;
+            this.btn_browser.Text = "Browser";
+            this.btn_browser.UseVisualStyleBackColor = true;
+            this.btn_browser.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(189, 449);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Smoothing";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(367, 449);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Shaperning";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox_src
+            // 
+            this.pictureBox_src.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox_src.Name = "pictureBox_src";
+            this.pictureBox_src.Size = new System.Drawing.Size(444, 320);
+            this.pictureBox_src.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_src.TabIndex = 3;
+            this.pictureBox_src.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(477, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(464, 320);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 509);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox_src);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_browser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_src)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_browser;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox_src;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ SpartialFilters/SpartialFilters/Sharpening.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartialFilters
+{
+  public  class Sharpening
+    {
+     
+   public static void Sharpen(Bitmap bmpInput, Bitmap bmpOutput)
+        {
+            Bitmap sharpenImage = (Bitmap)bmpInput.Clone();
+
+            int filterWidth = 3;
+            int filterHeight = 3;
+            int width = bmpInput.Width;
+            int height = bmpInput.Height;
+            int[,] arrGray = null;
+            int x, y, i, j;
+            double pixel_value = 0;
+            int new_value;
+
+            // Create sharpening filter
+            double[,] filter = new double[filterWidth, filterHeight];
+            filter[0, 0] = filter[0, 1] = filter[0, 2] = filter[1, 0] = filter[1, 2] = filter[2, 0] = filter[2, 1] = filter[2, 2] = 1;
+            filter[1, 1] = - 8;
+
+            double bias = 1.5;
+            Common common = new Common();
+            arrGray = common.ConvertToGray(sharpenImage);
+            for ( i = 1; i < width -1; i++)
+            {
+                for ( j = 1; j < height -1 ; j++)
+                {
+                    pixel_value = 0.0;
+                    for ( x = -1; x < filterWidth -1; x++)
+                    {
+                        for ( y = -1; y < filterHeight -1; y++)
+                        {
+                            pixel_value += filter[x + 1, y + 1] * arrGray[i + x, j + y];
+
+                        }
+                    }
+                    new_value = (int)(arrGray[i, j] - bias * pixel_value);
+                    if (new_value < 0) new_value = 0;
+                    if (new_value > 255) new_value = 255;
+                    bmpOutput.SetPixel(i, j , Color.FromArgb(new_value, new_value, new_value));
+                }
+            }
+           
+        }
+    }
+}
